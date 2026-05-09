c0e94657fb7bd60f698eddab4addda94ccee0ba9
RAHUL UJJAWAL <vikashkumarjyoti@VIKASHs-MacBook-Air.local>
2026-05-10 00:20:03 +0530

    add some names

---
+++ List.java
@@ -0,0 +1,16 @@
+package java-collections-masterclass;
+
+public class List {
+    public static void main(String[] args) {
+        
+    
+    List<String> names = new ArrayList<>();
+
+    names.add("Rahul");
+    names.add("Aman");
+    names.add("Rohit");
+
+    System.out.println(names);
+}
+    
+}
